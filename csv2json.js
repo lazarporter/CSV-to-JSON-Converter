ac280db4168e70b20b994d268dbb2c556d68da5c
Lazer Porter <lazar.porter@gmail.com>
2018-10-18 23:39:14 -0400

    tightened up the code, removed some extraneous code based on misunderstandings

--- csv2json.js
+++ csv2json.js
@@ -1,21 +1,15 @@
-//Count the commas until you hit the first newLine, adding them to keyArray. 
-//now you know an array of the keys.
-//You can iterate through the file, each time you hit a comma you know you are at the next value
-//every time you hit keyArray.length commas, you have completed a row.
-
 const fs = require('fs')
 const path = require('path')
 const csv = require('csvtojson')
 const filePath = path.join(__dirname, 'src', 'customer-data.csv')
 const outputFile = path.join(__dirname, 'src', 'outputfile.json')
 
-var buffer = ''
-var counter = 0
-csv().fromFile(filePath).then( (csvRow)=>{
-    buffer += csvRow;
-    fs.writeFile(outputFile, JSON.stringify(csvRow), ()=>{
-        console.log(`file ${++counter} written to file`)
+//read all data from the file into memory
+csv().fromFile(filePath).then( (allData)=>{
+    //This code only runs one time. This callback is called when the whole CSV is loaded into memory.    
+    fs.writeFile(outputFile, JSON.stringify(allData), ()=>{
+        console.log(allData)
+        console.log(`Converted to JSON and written to ${outputFile}`)
     })
 })
 
-console.log(JSON.stringify(buffer))
