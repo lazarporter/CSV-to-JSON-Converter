edec45115eb332f5cbf8f0253b22fad808f1f40f
Lazer Porter <lazar.porter@gmail.com>
2018-10-20 23:42:25 -0400

    added some comments

--- csv2json.js
+++ csv2json.js
@@ -6,20 +6,20 @@ const outputFile = path.join(__dirname, 'src', 'outputfile.json')
 
 converter = csv({
     //options object.  "trim:true" would trim off spaces surrounding column content
-    trim:false,
+    trim: false,
 });
 
 //read all data from the file into memory
-converter.fromFile(filePath).then( (allData)=>{
+converter.fromFile(filePath).then((allData) => {
     //This code only runs one time. This callback is called when all converted data is ready in memory    
-    
+
     //without JSON.stringify the output is just [object] [object] [object]..why?
-    fs.writeFile(outputFile, JSON.stringify(allData), ()=>{
-        
-        //this, without JSON.stringify, logs the objects printed beautifully..is that a functionality built into console.log?
-        console.log(allData)
-        console.log(`Converted to JSON and written to ${outputFile}`)
+    fs.writeFile(outputFile, (JSON.stringify(allData)), () => {
+        // console.log(allData) //works fine, prints as an array with JSON pretty format inside
+        // allData.forEach(element => {
+        //     console.log(element) //works fine, prints as JSON pretty format
+        // });
+
     })
 })
-
 //implement diff-ing the output and the solution data
